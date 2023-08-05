4ef55ff212edbb5d8817f4a0dbb6c7697ba72ea5
Sergey Engelke <sengle060@gmail.com>
2023-08-05 16:30:14 +0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #include "StackTemplate.h"
 
 int main(){
-    StackTemplate<int> stack;
-    stack.push(1);
-    stack.push(2);
-    StackTemplate<int> stack2(stack);
-//    stack.print();
-//    stack2.print();
-
-
-
-
-
-
     return 0;
 }
 
